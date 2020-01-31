b90d3f0764e2152a36f5496c4b5574b18dd125a5
ivan_ <ivan_@LAPTOP-LVTM11G4>
2020-01-31 19:19:55 +0100

    Hago cambios en Javadoc y vuelvo a generarlo con errores corregidos

--- EntradaBlogIZP/src/EntradaBlogIZP.java
+++ EntradaBlogIZP/src/EntradaBlogIZP.java
@@ -1,12 +1,12 @@
-/*Clase para manejar las entradas de un blog.
- * @author Ana López
+
+/**Clase para manejar las entradas de un blog.
+ * @author Iván Zarco
  * @since 21/3/1999
  * @version 2.3
  */
 
 public class EntradaBlogIZP {
-	//separador es el carácter que separa ENTRADA DE del
-	//nombre del autor
+	/**separador es el carácter que separa ENTRADA DE del nombre del autor*/
 	
 	public static char separador=':';
 	private int id;
@@ -16,9 +16,13 @@ public class EntradaBlogIZP {
 	public EntradaBlogIZP(int i, String string, String string2) {
 		// TODO Auto-generated constructor stub
 	}
-	//Constructor de la clase. recibe el número de entrada, el nombre del autor
-	//de la entrada y el texto que contiene la entrada. Si el número de entrada
-	//es negativo, lanza una excepción.
+	/**Constructor de la clase. 
+	*
+	* @param id Número de entrada
+	* @param autor Nombre del autor de la entrada
+	* @param texto Texto que contiene la entrada
+	* Si el número de entrada es negativo, lanza una excepción
+	*/
 	public void EntradaBlog(int id, String autor, String texto)throws IllegalArgumentException{
 		if(id<=0) throw new IllegalArgumentException("El id no puede ser negativo");
 		this.id=id;
@@ -33,17 +37,17 @@ public class EntradaBlogIZP {
 		return cad;
 	}
 	
-	//Devuelve el número de la entrada
+	/**Devuelve el número de la entrada*/
 	public int getId(){
 		return this.id;
 	}
 	
-	//devuelve el texto completo de la entrada
+	/**devuelve el texto completo de la entrada*/
 	public String getTexto(){
 		return this.texto;
 	}
 	
-	//devuelve el nombre del autor de la entrada en mayúsculas
+	/**devuelve el nombre del autor de la entrada en mayúsculas*/
 	public String getAutor(){
 		return this.autor.toUpperCase();
 	}
@@ -53,7 +57,7 @@ public class EntradaBlogIZP {
 		return this.autor;
 	}*/
 	
-	//No tiene porqué tener argumentos.
+	/**No tiene porqué tener argumentos*/
 	public static void main(String[] args) {
                 
 		EntradaBlogIZP e1= new EntradaBlogIZP (3,"Ana","Últimas noticias, está disponible BixBy 2.0");
